e580ac46231bf7909f98879561ff7914e5b54c73
robert <robert.kunz09@gmail.com>
2025-12-24 13:03:17 +0100

    Started Work on the next Party Member, Honeywisp

---
+++ scripts/data/actors/Honeywisp.lua
@@ -0,0 +1,96 @@
+local actor, super = Class(Actor, "Honeywisp")
+
+function actor:init()
+    super.init(self)
+    -- Display name (optional)
+    self.name = "Honeywisp"
+
+    -- Width and height for this actor, used to determine its center
+    self.width = 36
+    self.height = 60
+
+    -- Hitbox for this actor in the overworld (optional, uses width and height by default)
+    self.hitbox = { 0, 25, 19, 14 }
+
+    -- A table that defines where the Soul should be placed on this actor if they are a player.
+    -- First value is x, second value is y.
+    self.soul_offset = { 10, 24 }
+
+    -- Color for this actor used in outline areas (optional, defaults to red)
+    self.color = { 0, 1, 1 }
+
+    -- Path to this actor's sprites (defaults to "")
+    self.path = "party/Honeywisp/dark"
+    -- This actor's default sprite or animation, relative to the path (defaults to "")
+    self.default = "walk"
+
+    -- Sound to play when this actor speaks (optional)
+    self.voice = nil
+    -- Path to this actor's portrait for dialogue (optional)
+    self.portrait_path = "face/HW"
+    -- Offset position for this actor's portrait (optional)
+    self.portrait_offset = { -20, -10 }
+
+    -- Whether this actor as a follower will blush when close to the player
+    self.can_blush = false
+
+    -- Table of sprite animations
+    self.animations = {
+        ["slide"]               = { "slide", 4 / 30, true },
+        -- Battle animations
+        ["battle/idle"]         = { "battle/idle", 0.08, true },
+
+        ["battle/attack"]       = { "battle/attack", 1 / 10, false },
+        ["battle/act"]          = { "battle/act", 1 / 15, false },
+        ["battle/spell"]        = { "battle/spell", 0.12, false },
+        ["battle/item"]         = { "battle/item", 1 / 12, false, next = "battle/item_end" },
+        ["battle/spare"]        = { "battle/act", 1 / 15, false, next = "battle/idle" },
+
+        ["battle/attack_ready"] = { "battle/attackready", 0.08, false },
+        ["battle/act_ready"]    = { "battle/actready", 0.2, true },
+        ["battle/spell_ready"]  = { "battle/spellready", 0.08, false },
+        ["battle/item_ready"]   = { "battle/itemready", 0.2, true },
+        ["battle/defend_ready"] = { "battle/defend", 1 / 15, false },
+
+        ["battle/act_end"]      = { "battle/actend", 1 / 15, false, next = "battle/idle" },
+        ["battle/item_end"]     = { "battle/itemend", 1 / 15, false, next = "battle/idle" },
+
+        ["battle/hurt"]         = { "battle/hurt", 1 / 15, false, temp = true, duration = 0.5 },
+        ["battle/defeat"]       = { "battle/defeat", 1 / 15, false },
+        ["battle/vanish"]       = { "battle/vanish", 1 / 15, false },
+
+        ["battle/transition"]   = { "sword_jump_down", 0.2, true },
+        ["battle/intro"]        = { "battle/intro", 1 / 30, false },
+        ["battle/victory"]      = { "battle/victory", 1 / 10, false },
+    }
+
+    -- Table of sprite offsets (indexed by sprite name)
+    self.offsets = {
+        ["walk/left"] = { 0, 0 },
+        ["walk/right"] = { 0, 0 },
+        ["walk/up"] = { 0, 0 },
+        ["walk/down"] = { 0, 0 },
+        -- Battle offsets
+        ["battle/idle"] = { -5, -1 },
+
+        ["battle/attack"] = { -8, -6 },
+        ["battle/attackready"] = { -8, -6 },
+        ["battle/act"] = { -6, -6 },
+        ["battle/actend"] = { -6, -6 },
+        ["battle/actready"] = { -6, -6 },
+        ["battle/item"] = { -6, -6 },
+        ["battle/itemend"] = { -6, -6 },
+        ["battle/itemready"] = { -6, -6 },
+        ["battle/spell"] = { -6, -6 },
+        ["battle/spellready"] = { -6, -6 },
+        ["battle/defend"] = { -5, -1 },
+
+        ["battle/defeat"] = { -8, -5 },
+        ["battle/hurt"] = { -5, -1 },
+
+        ["battle/intro"] = { -8, -9 },
+        ["battle/victory"] = { -3, 0 },
+    }
+end
+
+return actor
---
+++ scripts/data/party/Honeywisp.lua
@@ -0,0 +1,108 @@
+local character, super = Class(PartyMember, "HW")
+
+function character:init()
+    super.init(self)
+    -- Display name
+    self.name = "HW"
+
+    -- Actor (handles overworld/battle sprites)
+    self:setActor("Honeywisp")
+
+    -- Display level (saved to the save file)
+    self.level = Game.chapter
+    -- Default title / class (saved to the save file)
+    self.title = "Honeywisp\nUses Eggs to\nsupport and heal."
+
+    -- Determines which character the soul comes from (higher number = higher priority)
+    self.soul_priority = 2
+    -- The color of this character's soul (optional, defaults to red)
+    self.soul_color = { 0, 1, 1 }
+
+    -- Whether the party member can act / use spells
+    self.has_act = false
+    self.has_spells = true
+
+    -- Whether the party member can use their X-Action
+    self.has_xact = true
+    -- X-Action name (displayed in this character's spell menu)
+    self.xact_name = "HW-action"
+
+    -- Spells
+    -- TBA
+    -- Current health (saved to the save file)
+    self.health = 50
+
+    -- Base stats (saved to the save file)
+    self.stats = {
+        health = 50,
+        attack = 2,
+        defense = 0,
+        magic = 10,
+    }
+
+    -- Max stats from level-ups
+    self.max_stats = {
+        health = 50
+    }
+
+    -- Weapon icon in equip menu
+    self.weapon_icon = "ui/menu/equip/sword"
+
+    -- Equipment (saved to the save file)
+    self:setWeapon("red_scarf")
+    self:setArmor(1, "amber_card")
+    --self:setArmor(2, "amber_card")
+
+    -- Default light world equipment item IDs (saves current equipment)
+    self.lw_weapon_default = "light/sword"
+    self.lw_armor_default = "light/bandage"
+
+    -- Character color (for action box outline and hp bar)
+    self.color = { 1, 0, 0.7 }
+    -- Damage color (for the number when attacking enemies) (defaults to the main color)
+    self.dmg_color = { 1, 0, 0.7 }
+    -- Attack bar color (for the target bar used in attack mode) (defaults to the main color)
+    self.attack_bar_color = { 1, 0, 0.7 }
+    -- Attack box color (for the attack area in attack mode) (defaults to darkened main color)
+    self.attack_box_color = { 0.9, 0, 0.6 }
+    -- X-Action color (for the color of X-Action menu items) (defaults to the main color)
+    self.xact_color = { 1, 0, 0.7 }
+
+    -- Head icon in the equip / power menu
+    self.menu_icon = "party/Honeywisp/head"
+    -- Path to head icons used in battle
+    self.head_icons = "party/Honeywisp/icon"
+    -- Name sprite
+    self.name_sprite = nil
+
+    -- Effect shown above enemy after attacking it
+    self.attack_sprite = "effects/attack/cut"
+    -- Sound played when this character attacks
+    self.attack_sound = "laz_c"
+    -- Pitch of the attack sound
+    self.attack_pitch = 1
+
+    function character:getGameOverMessage(main)
+        return {
+            "I-I'm sorry...",
+            "I d-didn't \nmean to...",
+            "P-please don't \ndie..."
+        }
+    end
+
+    -- Battle position offset (optional)
+    self.battle_offset = { 2, 1 }
+    -- Head icon position offset (optional)
+    self.head_icon_offset = { -5, -1 }
+    -- Menu icon position offset (optional)
+    self.menu_icon_offset = { 0, 0 }
+
+    -- Message shown on gameover (optional)
+    self.gameover_message = true
+end
+
+function character:autoHealAmount()
+    return math.huge
+end
+
+return character
